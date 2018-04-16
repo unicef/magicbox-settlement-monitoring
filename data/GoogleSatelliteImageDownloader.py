0e912308fc0a405d2db3cc1fa6574e59dd7ad885
soumyapatro <soumyachaitanya4315@gmail.com>
2018-04-16 10:41:40 -0700

    pr comment changes

--- data/GoogleSatelliteImageDownloader.py
+++ data/GoogleSatelliteImageDownloader.py
@@ -5,6 +5,7 @@
 # high resolution google satellite image
 
 import io
+import sys, os
 from urllib2 import Request, urlopen, URLError
 import  PIL
 from PIL import Image
@@ -13,7 +14,7 @@ from PIL import Image
 class GoogleSatelliteImageDownloader:
     """
         A class which generates high resolution google maps images given
-        a longitude, latitude and zoom level
+        a longitude, latitude and zoom level.
     """
 
     def __init__(self, lat, lng, zoom=12):
@@ -29,30 +30,31 @@ class GoogleSatelliteImageDownloader:
         self._lat = lat
         self._lng = lng
         self._zoom = zoom
-        print("I am here")
 
     
 
-    def getImage(self):
+    def get_image(self):
 
         """
-            Calls google static maps api to download satellite imagery at specified lat, long, zoom, scale, height & width
+            Calls google static maps api to download satellite imagery at specified lat, long,
+             zoom, scale, height & width. Please use your own google maps apikey. 
         """
 
         global apikey
-        apikey = "AIzaSyCkjcI42PXt1iL9cPHwk6T2p6ZXfOVFjKI"
+        apikey = ""
         width = 640
         height = 400
         image = Image.new('RGB', (width,height))
-        url = "https://maps.googleapis.com/maps/api/staticmap?maptype=satellite&center=%s,%s&zoom=%s&size=%sx%s&scale=2&key=%s" % (self._lat, self._lng, self._zoom,width,height,apikey)
+        url = "https://maps.googleapis.com/maps/api/staticmap?maptype=satellite&center=%s,%s&zoom=%s&size=%sx%s&scale=2&key=%s" \
+                % (self._lat, self._lng, self._zoom,width,height,apikey)
         print("url: "+url)
         request = Request(url)
         try:
             response = urlopen(request).read()
-            print("Received response")
+            print("Received response.")
             image = Image.open(io.BytesIO(response))
         except URLError, e:
-            print 'No images. Got an error code:', e
+            print 'No images. Got an error code:', str(e) +"."
         return image
 
 def main():
@@ -63,12 +65,13 @@ def main():
 
     try:
         # Get the high resolution image
-        img = gsid.getImage()
+        img = gsid.get_image()
     except IOError:
         print("Could not generate the image - try adjusting the zoom level and checking your coordinates")
     else:
         #Save the image to disk
-        img.save("satellite_image_"+str(latitude)+"_"+str(longitude)+".png")
+        current_path = os.path.dirname(sys.argv[0])
+        img.save(current_path+"/images/satellite_image_"+str(latitude)+"_"+str(longitude)+".png")
         print("The map has successfully been created")
 
 if __name__ == '__main__':  main()
